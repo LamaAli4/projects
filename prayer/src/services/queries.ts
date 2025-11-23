2322871db7d257d6c63444d555dbc697a2a91b15
Lama ALi <lama2015678@gmail.com>
2025-11-23 13:10:47 +0200

    Edit in the query

--- prayer/src/services/queries.ts
+++ prayer/src/services/queries.ts
@@ -1,22 +1,17 @@
-import { useQuery } from '@tanstack/react-query';
-import { 
-  fetchCountriesByContinent, 
-  fetchCitiesByCountry, 
-  fetchPrayerTimes, 
+import { useQuery } from "@tanstack/react-query";
+import {
+  fetchCountriesByContinent,
+  fetchCitiesByCountry,
+  fetchPrayerTimes,
   fetchPrayerMethods,
   type PrayerTime,
   type PrayerMethod,
-  type Country
-} from './api';
-
-const COUNTRIES_QUERY_KEY = 'countries';
-const CITIES_QUERY_KEY = 'cities';
-const PRAYER_TIMES_QUERY_KEY = 'prayerTimes';
-const PRAYER_METHODS_QUERY_KEY = 'prayerMethods';
+  type Country,
+} from "./api";
 
 export const useCountries = (continent: string) => {
   return useQuery<Country[]>({
-    queryKey: [COUNTRIES_QUERY_KEY, continent],
+    queryKey: ["countries", continent],
     queryFn: () => fetchCountriesByContinent(continent),
     enabled: !!continent,
     staleTime: Infinity,
@@ -25,25 +20,28 @@ export const useCountries = (continent: string) => {
 
 export const useCities = (country: string) => {
   return useQuery<string[]>({
-    queryKey: [CITIES_QUERY_KEY, country],
+    queryKey: ["cities", country],
     queryFn: () => fetchCitiesByCountry(country),
     enabled: !!country,
     staleTime: Infinity,
   });
 };
 
-export const usePrayerTimes = (city: string, country: string, method: number) => {
+export const usePrayerTimes = (
+  city: string,
+  country: string,
+  method: number
+) => {
   return useQuery<PrayerTime>({
-    queryKey: [PRAYER_TIMES_QUERY_KEY, city, country, method],
+    queryKey: ["prayerTimes", city, country, method],
     queryFn: () => fetchPrayerTimes(city, country, method),
     enabled: !!city && !!country && !!method,
-    refetchInterval: 60000, 
   });
 };
 
 export const usePrayerMethods = () => {
   return useQuery<PrayerMethod[]>({
-    queryKey: [PRAYER_METHODS_QUERY_KEY],
+    queryKey: ["prayerMethods"],
     queryFn: fetchPrayerMethods,
     staleTime: Infinity,
   });
